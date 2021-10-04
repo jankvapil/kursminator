9b42594f05391e718ac86cb45e1493e9f7dcc23c
corey6988 <schillermare@gmail.com>
2021-10-04 11:47:30 +0200

    added assets, basic header

--- web/components/common/Header.js
+++ web/components/common/Header.js
@@ -1,10 +1,25 @@
+import { Layout, Menu, Breadcrumb } from 'antd';
+import 'antd/dist/antd.css';
+
 
 ///
 /// Header component
 ///
 const Header = () => {
   return (
-    <header></header>
+    <>
+      <Layout className="layout">
+        <Layout.Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            {new Array(15).fill(null).map((_, index) => {
+              const key = index + 1;
+              return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+            })}
+          </Menu>
+        </Layout.Header>
+      </Layout>
+    </>
   )
 }
 
