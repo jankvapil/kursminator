b4a6328a3ed7ed45d396568350316f266c157ddb
Petr Unzeitig <Petr.Unzeitig@tescosw.cz>
2021-10-05 13:35:14 +0200

    add queries

--- api/GraphQL/Query.cs
+++ api/GraphQL/Query.cs
@@ -27,9 +27,41 @@ namespace CourseApi.GraphQL
         [UseDbContext(typeof(AppDbContext))]
         [UseProjection]
         [UseFiltering]
+        public IQueryable<Place> GetPlaces([ScopedService] AppDbContext context)
+        {
+            return context.Places;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        [UseFiltering]
         public IQueryable<User> GetUsers([ScopedService] AppDbContext context)
         {
             return context.Users;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        [UseFiltering]
+        public IQueryable<Role> GetRoles([ScopedService] AppDbContext context)
+        {
+            return context.Roles;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        [UseFiltering]
+        public IQueryable<UserCourseFavourite> GetUserCourseFavourites([ScopedService] AppDbContext context)
+        {
+            return context.UserCourseFavourites;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        [UseFiltering]
+        public IQueryable<UserCourseReservation> UserCourseReservations([ScopedService] AppDbContext context)
+        {
+            return context.UserCourseReservations;
         }
     }
 }
