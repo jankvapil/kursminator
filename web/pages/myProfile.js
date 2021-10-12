a94a5fafb92d044baf10c1333d3460950e1603c7
Jan Kvapil <jkvapil6@gmail.com>
2021-10-12 12:47:38 +0200

    fb login

--- web/pages/myProfile.js
+++ web/pages/myProfile.js
@@ -1,14 +1,46 @@
 
-import Content from '../components/common/Content'
+import Content from '@/components/common/Content'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 ///
 /// My profile page includes favourite courses, personal info, 
 ///  credit history and past/next courses overview 
 ///
 export default function myProfilePage() {
+  const [ls, setLs] = useState(null)
+  useEffect(() => {
+    setLs(window.localStorage)
+  }, [])
+
+  const myLoader = ({ src, width, quality }) => {
+    if (!ls)
+      return src
+    else return ls.getItem("pictureUrl")
+  }
+
+  const showLocalStorage = () => {
+    console.log(localStorage)    
+  }
+
   return (
     <Content>
-      My profile page
+      {
+        ls && ls.getItem("isLogged") === 'true' ? (
+          <>
+            <Image
+              loader={myLoader}
+              src="https://www.pngfind.com/pngs/m/610-6104451_image-placeholder-png-user-profile-placeholder-image-png.png"
+              alt="Picture of the author"
+              width={50}
+              height={50}
+            />
+            My profile page
+            <button onClick={showLocalStorage}>My Profile</button>
+          </>
+        ) : (<h1>Sorry, you are not logged in.</h1>)
+      }
+      
     </Content>
   )
 }
