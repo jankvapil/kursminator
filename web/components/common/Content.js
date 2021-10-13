541f5213a00cf4ee0f4c507aa6b5c756b522140b
Martin Pifka <martin.pifka@configair.com>
2021-10-13 16:07:33 +0200

    Header, Footer, Edit Content, Calendar

--- web/components/common/Content.js
+++ web/components/common/Content.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 
+import Header from './Header'
 import Footer from './Footer'
 
-  
 
 ///
 /// Content component
@@ -10,17 +10,20 @@ import Footer from './Footer'
 const Content = (props) => {
 
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Kurzminator</title>
-        <link rel="icon" href="/logokursm-light.ico" />
-      </Head>
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        { props.children }
-      </main>
-      <Footer />
-    </div>
+    return (
+        <>
+            <Header/>
+            <div className="flex flex-col items-center justify-center min-h-screen py-2">
+                <Head>
+                    <title>Kurzminator</title>
+                    <link rel="icon" href="/logokursm-light.ico" />
+                </Head>
+                <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+                    { props.children }
+                </main>
+            </div>
+            <Footer />
+        </>
   )
 }
 
--- web/components/common/Header.js
+++ web/components/common/Header.js
@@ -1,4 +1,5 @@
-import { Layout, Menu, Breadcrumb } from 'antd';
+﻿import { Layout, Button, Input } from 'antd';
+import { CalendarTwoTone } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 
 
@@ -6,21 +7,36 @@ import 'antd/dist/antd.css';
 /// Header component
 ///
 const Header = () => {
-  return (
-    <>
-      <Layout className="layout">
-        <Layout.Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            {new Array(15).fill(null).map((_, index) => {
-              const key = index + 1;
-              return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
-            })}
-          </Menu>
-        </Layout.Header>
-      </Layout>
-    </>
-  )
+    return (
+        <>
+            <Layout className="layout">
+                <Layout.Header className="flex justify-around items-center">
+                    <div className="flex items-center mt-1">
+                        <div>
+                            <img src="/logokursm-light.ico" alt="logo" width="50" height="60" />
+                        </div>
+                        <p className="text-3xl text-white mb-0 ml-4 font-mono">Kursminator</p>
+                    </div>
+                    <Button type="link">Kurzy</Button>
+                    <div className="w-1/2">
+                        <Input placeholder="Hledaný kurz..." />
+                    </div>
+                    <div className="flex items-center">
+                        <CalendarTwoTone twoToneColor="#fff" />
+                        <p className="text-white m-0 ml-2">Kontakt</p>
+                    </div>
+                    <div className="flex items-center">
+                        <CalendarTwoTone twoToneColor="#fff" />
+                        <p className="text-white m-0 ml-2">Kalendář</p>
+                    </div>
+                    <div>
+                        <Button className="mr-5" type="primary">Přihlášení</Button>
+                        <Button>Registrace</Button>
+                    </div>
+                </Layout.Header>
+            </Layout>
+        </>
+    )
 }
 
 export default Header
