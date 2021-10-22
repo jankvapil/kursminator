7fefbef89e81fa21b76cb5425c94259ccbdfb74f
Petr Unzeitig <Petr.Unzeitig@tescosw.cz>
2021-10-22 10:00:27 +0200

    fix typo Virtual

---
+++ api/Migrations/20211022075321_Virtual typo fix.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class Virtualtypofix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Virutal",
+                table: "Places",
+                newName: "Virtual");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Virtual",
+                table: "Places",
+                newName: "Virutal");
+        }
+    }
+}
