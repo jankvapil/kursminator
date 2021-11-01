8ebb72d91688413933142f5b081bbe6011d5eea3
Jan Kvapil <jkvapil6@gmail.com>
2021-11-01 21:31:10 +0100

    my courses table

--- web/pages/myProfile.js
+++ web/pages/myProfile.js
@@ -5,12 +5,15 @@ import { Row, Col } from 'antd'
 import { getUserInfo } from '@/core/graphql/queries/userQueries'
 import { fetchAllCourses } from '@/core/graphql/queries/coursesQueries'
 import { Table, Tag, Space } from 'antd'
+import { courses } from '@/core/actions'
+import { useRouter } from 'next/router'
 
 ///
 /// My profile page includes favourite courses, personal info, 
 ///  credit history and past/next courses overview 
 ///
 export default function myProfilePage(props) {
+  const router = useRouter()
   const [currentUser, setCurrentUser] = useState(null)
   const [favouriteCourses, setFavouriteCourses] = useState([])
   const [ls, setLs] = useState(null)
@@ -24,6 +27,7 @@ export default function myProfilePage(props) {
 
   const loadUserInfo = async () => {
     const res = await getUserInfo()
+    // console.log(res)
     setCurrentUser(res)
   }
 
@@ -32,30 +36,40 @@ export default function myProfilePage(props) {
       {
         ls && ls.getItem("isLogged") === 'true' && currentUser ? (
           <Row className="min-h-screen">
-            <Col span={6} className="my-12">
+            <Col span={6} className="my-10">
               <h1 className="text-3xl text-center mb-8">{ currentUser.name } { currentUser.surname }</h1>
-              <img className="h-32 w-32 rounded-full mx-auto" src={ls.getItem("pictureUrl")}></img>
+              <img className="h-32 w-32 rounded-full mx-auto" src={ currentUser.photoUrl }></img>
 
               <span className="block w-full text-center my-8 text-2xl"> { currentUser.credits } kreditů </span>
               
-              {/* <button onClick={showLocalStorage}>My Profile</button>
-              <button className="text-xl m-6" onClick={() => localStorage.clear()}>logout</button> */}
+              {/* <button onClick={showLocalStorage}>My Profile</button> */}
+              {/* <button className="text-xl m-6" onClick={() => localStorage.clear()}>logout</button> */}
             </Col>
             <Col span={18}>
               
               <section className="mt-10 mr-4">
-                <h2 className="text-2xl mb-8">Oblíbené kurwy</h2>
-                <Table 
+                <h2 className="text-2xl mb-8 pt-1">Moje kurzy</h2>
+                <Table
+                  rowKey="id"   
                   columns={[
                     {
-                      title: 'Name',
-                      dataIndex: 'name',
-                      key: 'name',
+                      title: 'Název',
+                      dataIndex: 'id',
+                      render: id => <button onClick={() => router.push(`courseDetail?id=${id}`)}>{favouriteCourses.filter(c => c.id == id)[0].name}</button>,
                     },
                     {
-                      title: 'Price',
+                      title: 'Datum',
+                      dataIndex: 'date',
+                      render: text => text.substring(0,10)
+                    },
+                    {
+                      title: 'Status',
+                      dataIndex: 'id',
+                      render: text => <span className="bg-blue-200 rounded-md py-1 px-1.5">Rezervovaný</span>
+                    },
+                    {
+                      title: 'Cena',
                       dataIndex: 'price',
-                      key: 'id',
                     }]} 
                   dataSource={favouriteCourses} />
               </section>
