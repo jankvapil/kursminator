ea091222658072d51a15e89d20370a2c40aba55a
Jan Kvapil <jkvapil6@gmail.com>
2023-12-12 21:50:00 +0100

    Change api url using https proxy

--- web/core/config.js
+++ web/core/config.js
@@ -1,6 +1,8 @@
 
 // export const GRAPHQL_API = "http://localhost:5000/graphql"
 
-export const GRAPHQL_API = "http://kursminator.aspifyhost.cz/graphql/"
+// export const GRAPHQL_API = "http://kursminator.aspifyhost.cz/graphql/"
+
+export const GRAPHQL_API = "https://cors-anywhere.herokuapp.com/http://kursminator.aspifyhost.cz/graphql/"
 
 // export const GRAPHQL_API = "https://kursminator.azurewebsites.net/graphql/"
